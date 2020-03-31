8a6ddc9f8e317536c26dd99a292c1efe527fc028
AymanSulaiman <saymansulaiman@gmail.com>
2020-03-30 22:09:47 -0700

    This is all the work that we have done thus far.  Created the scema and nearly finished with the transformation.

---
+++ README.md
@@ -0,0 +1,3 @@
+ETL Project
+
+link `https://data.humdata.org/dataset/novel-coronavirus-2019-ncov-cases`
---
+++ schema.sql
@@ -0,0 +1,32 @@
+﻿-- Exported from QuickDBD: https://www.quickdatabasediagrams.com/
+-- Link to schema: https://app.quickdatabasediagrams.com/#/d/I8hZjF
+-- NOTE! If you have used non-SQL datatypes in your design, you will have to change these here.
+
+
+CREATE TABLE "confirmed" (
+    "country_name" VARCHAR   NOT NULL,
+    "date" DATE   NOT NULL,
+    "number_of_confirmed" INT   NOT NULL,
+    CONSTRAINT "pk_confirmed" PRIMARY KEY (
+        "country_name"
+     )
+);
+
+CREATE TABLE "recovered" (
+    "country_name" VARCHAR   NOT NULL,
+    "date" DATE   NOT NULL,
+    "number_of_recovered" INT   NOT NULL
+);
+
+CREATE TABLE "death" (
+    "country_name" VARCHAR   NOT NULL,
+    "date" DATE   NOT NULL,
+    "number_of_deaths" INT   NOT NULL
+);
+
+ALTER TABLE "recovered" ADD CONSTRAINT "fk_recovered_country_name_date" FOREIGN KEY("country_name", "date")
+REFERENCES "confirmed" ("country_name", "date");
+
+ALTER TABLE "death" ADD CONSTRAINT "fk_death_country_name_date" FOREIGN KEY("country_name", "date")
+REFERENCES "confirmed" ("country_name", "date");
+
