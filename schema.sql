f436316a0a493ee66fb190f6f9059e544337ee72
AymanSulaiman <saymansulaiman@gmail.com>
2020-04-01 17:33:48 -0700

    fixed the loading part now we can get on with the analysis

--- schema.sql
+++ schema.sql
@@ -11,38 +11,37 @@ DROP TABLE IF EXISTS "recovered";
 DROP TABLE IF EXISTS "death";
 
 CREATE TABLE "confirmed" (
-    "country_name" VARCHAR   NOT NULL,
+    "country" VARCHAR   NOT NULL,
     "date" DATE   NOT NULL,
     "accumulated_confirmed" INT   NOT NULL,
     "increase_of_confirmed" INT   NOT NULL
 );
 
 CREATE TABLE "recovered" (
-    "country_name" VARCHAR   NOT NULL,
+    "country" VARCHAR   NOT NULL,
     "date" DATE   NOT NULL,
     "accumulated_recovered" INT   NOT NULL,
     "increase_of_recovered" INT   NOT NULL
 );
 
 CREATE TABLE "death" (
-    "country_name" VARCHAR   NOT NULL,
+    "country" VARCHAR   NOT NULL,
     "date" DATE   NOT NULL,
-    "accumulated_death" INT   NOT NULL,
-    "increase_of_death" INT   NOT NULL
+    "accumulated_deaths" INT   NOT NULL,
+    "increase_of_deaths" INT   NOT NULL
 );
 
--- Sanity check, all of the same length
+-- Sanity checks post loading data from the jupyter notebook
 SELECT COUNT(*) from recovered;
 SELECT COUNT(*) FROM death;
 SELECT COUNT(*) FROM confirmed;
 
 
 
-
--- This Doen't work
+-- This Doesn't work
 -- ALTER TABLE "recovered" ADD CONSTRAINT "fk_recovered_country_name_date" FOREIGN KEY("country_name", "date")
 -- REFERENCES "confirmed" ("country_name", "date");
 
 -- ALTER TABLE "death" ADD CONSTRAINT "fk_death_country_name_date" FOREIGN KEY("country_name", "date")
 -- REFERENCES "confirmed" ("country_name", "date");
--- This Doen't work
+-- This Doesn't work
